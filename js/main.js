9d7e98c9b2065358adf54ccd485f02ce710393de
aryanataandi <a.ryan.ata.ap@gmail.com>
2021-02-15 19:00:07 +0700

    More responsive navbar

--- js/main.js
+++ js/main.js
@@ -7,22 +7,38 @@ AOS.init({
 
 // * Scroll to top
 let scrollButton = document.getElementById("scroll-top");
-
-window.onscroll = function() {
-    scrollFunction()
-};
+let navbar = document.getElementById("navbar");
 
 function scrollFunction() {
     if (document.body.scrollTop > 70 || document.documentElement.scrollTop > 70) {
         scrollButton.style.opacity = "100";
         scrollButton.style.cursor = "pointer";
+        navbar.classList.add("shadow");
     } else {
         scrollButton.style.opacity = "0";
         scrollButton.style.cursor = "default";
+        navbar.classList.remove("shadow");
     }
 }
 
 function topFunction() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
+}
+
+// * Responsive navbar
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  scrollFunction();
+  responsiveNav();
+}
+
+function responsiveNav() {
+    var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-100px";
+  }
+  prevScrollpos = currentScrollPos;
 }
