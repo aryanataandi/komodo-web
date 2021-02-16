088d168dc83cee5d664edf77c048772762498697
aryanataandi <a.ryan.ata.ap@gmail.com>
2021-02-16 22:56:51 +0700

    Added section 6

--- js/main.js
+++ js/main.js
@@ -38,8 +38,10 @@ function responsiveNav() {
     var currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
     document.getElementById("navbar").style.top = "0";
+    document.getElementById("navbar").style.opacity = "1";
   } else {
     document.getElementById("navbar").style.top = "-100px";
+    document.getElementById("navbar").style.opacity = "0";
   }
   prevScrollpos = currentScrollPos;
 }
